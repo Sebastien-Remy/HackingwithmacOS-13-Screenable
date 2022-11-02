94ce4e720eabe2aad8c293556aaaf1750617eeab
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-02 07:54:12 +0100

    Initial Commit

---
+++ Screenable/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Screenable
+//
+//  Created by Sebastien REMY on 02/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Screenable/ScreenableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenableApp.swift
+//  Screenable
+//
+//  Created by Sebastien REMY on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScreenableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
